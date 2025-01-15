ef2d2b10e7cd3673bb2c76776bf4a192fc9720b2
VictorFioravanteXavier <157313401+VictorFioravanteXavier@users.noreply.github.com>
2025-01-15 15:57:57 -0300

    ex 003 e 004

---
+++ 03/index.ts
@@ -0,0 +1,147 @@
+const faturamento: Array<{dia: number, valor: number}> = [
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
+]
+
+let maior: number = faturamento[0].valor;
+let menor: number = faturamento[0].valor;
+let media_faturamento: number = 0;
+let dias_validos: number = 0;
+
+faturamento.forEach(element => {
+    if (element.valor > maior){
+        maior = element.valor
+    } else if (element.valor < menor){
+        menor = element.valor
+    }
+    if (!(element.valor <= 0)){
+        media_faturamento += element.valor
+        dias_validos++
+    }
+})
+
+media_faturamento = media_faturamento / dias_validos;
+
+const dias_maior_media_faturamento = faturamento
+    .filter((element) => element.valor > media_faturamento)
+    .map((element) => element.dia);
+
+console.log(maior, menor, dias_maior_media_faturamento)
---
+++ 04/index.ts
@@ -0,0 +1,38 @@
+let distribuidora: Array<{estado: string, valor: number}> = [
+    {
+        estado: "SP",
+        valor: 67836.43
+    },
+    {
+        estado: "RJ",
+        valor: 36678.66
+    },
+    {
+        estado: "RJ",
+        valor: 36678.66
+    },
+    {
+        estado: "MJ",
+        valor: 29229.88
+    },
+    {
+        estado: "ES",
+        valor: 27165.48
+    },
+    {
+        estado: "Outros",
+        valor: 19849.53
+    }
+]
+
+let total_fatuarmento_distribuidora: number = 0;
+
+
+distribuidora.forEach((element) => {
+    total_fatuarmento_distribuidora += element.valor;
+})
+
+distribuidora.forEach(element => {
+    const pocentagem = (element.valor/total_fatuarmento_distribuidora)*100
+    console.log(`${element.estado} - ${pocentagem}`)
+})
